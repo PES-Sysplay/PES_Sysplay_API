2d3415209f1efde7218f231f84a2a1b35646d806
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2021-03-13 16:19:31 +0100

    typo on model

--- activity/models.py
+++ activity/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Activity(models.Model):
     name = models.CharField(primary_key=True, max_length=20)
+
+    class Meta:
+        verbose_name_plural = 'Activities'
