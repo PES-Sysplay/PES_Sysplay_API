f58fab2b4ee60adeb3fec05ba08d4db7f2e19e57
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2021-03-25 19:21:40 +0100

    static fix final

--- workout/settings_heroku.py
+++ workout/settings_heroku.py
@@ -1,11 +1,5 @@
 from workout.settings import *
 
-import dj_database_url
-
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
-
 DOMAIN = os.environ.get('DOMAIN', None)
 HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME', None)
 if HEROKU_APP_NAME and not DOMAIN:
@@ -14,8 +8,8 @@ elif not DOMAIN:
     DOMAIN = 'localhost:8000'
 ALLOWED_HOSTS.append(DOMAIN)
 
-# MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', None),
