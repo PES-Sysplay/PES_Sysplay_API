97b8c5f1700c2e215db1db0e229957dfabee99ef
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2021-03-25 11:20:51 +0100

    Changed time output api

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
 from activity.models import Activity
@@ -6,6 +8,11 @@ from activity.models import Activity
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     photo_url = serializers.SerializerMethodField()
     status = serializers.SerializerMethodField()
+    date_time = serializers.SerializerMethodField()
+
+    def get_date_time(self, activity):
+        date_time = datetime.combine(activity.start_date, activity.start_time)
+        return date_time.strftime("%b %d, %Y, %H:%M")
 
     def get_status(self, activity):
         return activity.get_status_display()
@@ -17,5 +24,5 @@ class ActivitySerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Activity
-        fields = ['id', 'name', 'description', 'photo_url', 'activity_type_id', 'start_date', 'start_time', 'duration',
+        fields = ['id', 'name', 'description', 'photo_url', 'activity_type_id', 'date_time', 'duration',
                   'normal_price', 'member_price', 'number_participants', 'status', 'location', 'only_member']
